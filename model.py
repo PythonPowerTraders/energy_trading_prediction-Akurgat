b41793a70270ea01f60dfcd5a0510be74de69472
Andrew Kurgat <akurgat@gmail.com>
2020-01-23 23:20:49 +0300

    Added price ML function and model.

--- model.py
+++ model.py
@@ -7,6 +7,7 @@ from analysis import technical_analysis, indications
 #Declare models
 distinct_model = load_model("models\buy_sell_models\model-distinct.h5")
 general_model = load_model("models\buy_sell_models\model-general.h5")
+price_prediction_model = load_model("models\price_prediction_models\price_model.h5")
 
 
 def buy_sell_regions(df, model): #Function uses the raw data to identify the general by and sell regions 
@@ -57,3 +58,26 @@ def distinct_buy_sell(data, model): #Identifies distinct buy sell point in the d
     y = ohe.fit_transform(data[['Distinct_Action']])#Converting the targets to a form sutable for the ML to interpate diferent categories
     decoded = ohe.inverse_transform(results)#Convert the prediction array back to the respective categories
     print (decoded[-1]) #Return the prediction for the most current data point
+
+def price_prediction(df, model):
+    #Load at least 90 data point from the returned historical data for analysis
+    df = df.iloc[-90:]
+    technical_analysis(df, df['Close'], df['High'], df['Low'])#Conduct the necessary technical and indication calculations
+    indications(df)
+    
+    #Define the parameter used for prediction and drop the unnecessary columns in the data
+    X = np.array(df.drop(['Close', 'P_Past', 'R1_Past', 'R2_Past', 'S1_Past', 'S2_Past', '%K', '%D', 'RSI', 
+                          'MACD', 'MACDS', 'MACDH', 'Action_Buy', 'Action_Hold', 'Action_Sell', 'General_Action', 
+                          'Distinct_Action'], 1))
+    y = np.array(df[['Close']])
+    
+     #Scales the data to make it easier for the ML to identify the desired partterns
+    X = scale(X)
+    scaler = StandardScaler()
+    X = scaler.fit_transform(X)
+    y = scaler.fit_transform(y.reshape(-1, 1))
+    
+    #Feed the features to the general ML model to get the prediction
+    results = model.predict(X).round(2)
+    results = scaler.inverse_transform(results)#Convert the prediction array back to the respective price value
+    print (results[-1].round(2)) #Display current price prediction and round off to 2 decimal places
