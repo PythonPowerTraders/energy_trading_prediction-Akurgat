6f6309903760235fccc096458da4fd5b337a72d1
Andrew Kurgat <akurgat@gmail.com>
2020-01-23 23:26:08 +0300

    Fixed typo.

--- model.py
+++ model.py
@@ -17,7 +17,8 @@ def buy_sell_regions(df, model): #Function uses the raw data to identify the gen
     technical_analysis(df, df['Close'], df['High'], df['Low'])
     indications(df)
     
-    ohe = OneHotEncoder(categories = [['Buy', 'Hold', 'Sell']], sparse = False)#Define the targeted categories before conversion to a sutable form for the ML model
+    ohe = OneHotEncoder(categories = [['Buy', 'Hold', 'Sell']], sparse = False)#Define the targeted categories before conversion 
+                                                                                #to a sutable form for the ML model
     #Define the parameter used for prediction and drop the unnecessary columns in the data
     X = np.array(df.drop(['Open', 'High', 'Low', 'Close', 'P', 'R1', 'R2', 'S1', 'S2', 'P_Past', 
                         'R1_Past', 'R2_Past', 'S1_Past', 'S2_Past', 'Action_Buy', 'Action_Hold', 
@@ -33,16 +34,19 @@ def buy_sell_regions(df, model): #Function uses the raw data to identify the gen
     #Feed the features to the general ML model to get the prediction
     results = model.predict(X).round(1)
     decoded = ohe.inverse_transform(results) #Convert the prediction array back to the respective categories
-    return decoded.reshape(-1), df #Reshape the prediction array to a 1D matrix and return it together with the data used before the prediction
+    return decoded.reshape(-1), df #Reshape the prediction array to a 1D matrix and return it together with the data used 
+    before the prediction
 
 def distinct_buy_sell(data, model): #Identifies distinct buy sell point in the data based on the buy sell regions predicted above
-    #Data is the dataframe returned from the previous model
+    #data is the dataframe returned from the previous model
     df = pd.DataFrame()#Creating a and feeding the predictions from the general model to it
-    ohe = OneHotEncoder(categories = [['Buy', 'Hold', 'Sell']], sparse = False)#Define the targeted categories before conversion to a sutable form for the ML model
+    ohe = OneHotEncoder(categories = [['Buy', 'Hold', 'Sell']], sparse = False)#Define the targeted categories before 
+    conversion to a sutable form for the ML model
     signals, data = buy_sell_regions(data, general_model)
     df['General_Action'] =  (signals)
 
-    #Analysing predictions from the identified regions to develop suitable feature for the distinct model and saving them to the dataframe
+    #Analysing predictions from the identified regions to develop suitable feature for the distinct model and saving 
+    # them to the dataframe
     df.loc[((df['General_Action'] == 'Buy') & (df['General_Action'].shift(-3) == 'Sell')), 'Action_Buy'] = 1
     df.loc[((df['General_Action'] == 'Sell') & (df['General_Action'].shift(-3) == 'Buy')), 'Action_Sell'] = 1
     df.loc[((df['General_Action'] == 'Hold')), 'Action_Hold'] = 1
@@ -55,7 +59,8 @@ def distinct_buy_sell(data, model): #Identifies distinct buy sell point in the d
     #Converting th remaining features to a form suitable for prediction
     X = np.array(df)
     results = model.predict(X).round(1)#Feeding the features to the general ML model to get the prediction
-    y = ohe.fit_transform(data[['Distinct_Action']])#Converting the targets to a form sutable for the ML to interpate diferent categories
+    y = ohe.fit_transform(data[['Distinct_Action']])#Converting the targets to a form sutable for the 
+                                                    #ML to interpate diferent categories
     decoded = ohe.inverse_transform(results)#Convert the prediction array back to the respective categories
     print (decoded[-1]) #Return the prediction for the most current data point
 
