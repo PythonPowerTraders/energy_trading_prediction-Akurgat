42792bc57c0cc420cee1f1781fd566e00c418be7
Andrew Kurgat <akurgat@gmail.com>
2020-03-06 00:28:33 +0300

    Fixed reverse trading signals bug

--- model.py
+++ model.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import numpy as np
-from sklearn.preprocessing import scale, StandardScaler, OneHotEncoder
+from sklearn.preprocessing import scale, StandardScaler, OneHotEncoder, LabelEncoder
 from tensorflow.keras.models import load_model
 from analysis import technical_analysis, indications
+from tensorflow.keras.utils import to_categorical
+
 
 #Declare models
 buy_sell_prediction_model = load_model("models\model_buy_sell_.h5")
@@ -26,7 +28,13 @@ def buy_sell_prediction(df, model): #Does all the buy sell indications used by t
     #Define the parameter used for prediction
     X = np.array(df[['Action_Buy', 'Action_Hold', 'Action_Sell']])
     #Converting the targets to a form sutable for the ML to interpate diferent categories
-    y = ohe.fit_transform(df[['Distinct_Action']])
+    #y = ohe.fit_transform(df[['Distinct_Action']])
+
+    le = LabelEncoder()
+    le = le.fit(['Buy', 'Hold', 'Sell'])
+    y = le.transform(df[['Distinct_Action']])
+    y = to_categorical(y)
+    
     
     #Scales the data to make it easier for the ML to identify the desired patterns
     X = scale(X)
@@ -37,7 +45,10 @@ def buy_sell_prediction(df, model): #Does all the buy sell indications used by t
     #Feed the features to the general ML model to get the prediction
     results = model.predict(X).round(1)
     #Converting the targets to a form sutable for the ML to interpate diferent categories
-    decoded = ohe.inverse_transform(results)
+    #clear
+    # decoded = ohe.inverse_transform(results)
+    decoded =le.inverse_transform(np.argmax(results.round(1), axis = 1))
+
     return decoded#Return the prediction for the most current data point
 
 def price_prediction(df, model):
